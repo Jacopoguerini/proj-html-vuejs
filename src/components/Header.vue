940bfe48f1665b023692c1b1a9d3cc04f4cb664a
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-06-21 13:19:13 +0200

    Bozza iniziale nav

---
+++ src/components/Header.vue
@@ -0,0 +1,133 @@
+<template>
+  <header>
+      <div class="info-bar">
+          <div class="container flex-spbt-centre">
+            <!-- opening times -->
+            <div class="nav-opening-times">
+                <i class="fas fa-clock"></i>
+                <span>
+                  Open Hours: {{ openingTimes }}
+                </span>
+            </div>
+            <!-- /opening times -->
+
+            <!-- contacts -->
+            <ul class="nav-contacts list-inline">
+                <!-- phone number -->
+                <li>
+                    <i class="fas fa-phone-alt"></i>
+                    <span>{{ phone }}</span>
+                </li>
+                <!-- /phone number -->
+
+                <!-- mail -->
+                <li>
+                    <i class="fas fa-envelope"></i>
+                    <span>{{ contactMail }}</span>
+                </li>
+                <!-- /mail -->
+
+                <!-- social contacts -->
+                <li>
+                    <a href="#">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+                </li>
+                <!-- /social contacts -->
+            </ul>
+            <!-- /contacts -->
+
+
+          </div>
+      </div>
+
+      <Nav 
+      :navLinks="navLinks" />
+      
+  </header>
+</template>
+
+<script>
+import Nav from './Nav';
+
+export default {
+    name: "Header",
+    data: function() {
+        return {
+            openingTimes: "Mon - Sat - 9:00 - 18:00",
+            phone: "+1 (305) 1234-5678" ,
+            contactMail: "Hello@example.com",
+            navLinks: [
+                {
+                    name: "Home",
+                    url: ""
+                },
+                {
+                    name: "Services",
+                    url: ""
+                },
+                {
+                    name: "About",
+                    url: ""
+                },
+                {
+                    name: "Projects",
+                    url: ""
+                },
+                {
+                    name: "Results",
+                    url: ""
+                }
+            ]
+        }
+    },
+    components: {
+        Nav
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/general.scss';
+@import '../style/variables.scss';
+
+header {
+    height: 100px;
+
+    .info-bar {
+        height: 40px;
+        background-color: $bigStone;
+        color: $pumice;
+        display: flex;
+        align-items: center;
+        font-size: 11px;
+
+        a {
+            color: $pumice;
+        }
+
+        .nav-contacts {
+            & > li {
+                margin: 0 15px;
+
+                i {
+                    margin: 0 5px;
+                }
+            }
+        }
+    }
+    
+}
+
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,62 @@
+<template>
+    <nav>
+        <div class="container flex-spbt-centre">
+
+            <!-- logo image -->
+            <div class="image-logo">
+                <a href="#">
+                    <img src="../assets/logo.png" alt="NexGen Logo">
+                </a>
+            </div>
+            <!-- /logo image -->
+
+            <ul class="navbar list-inline">
+                <li v-for="link in navLinks" :key="link.name">
+                    <a :href="link.url">
+                        {{ link.name }}
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+
+export default {
+    name: "Nav",
+    props: {
+        "navLinks": Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/general.scss';
+@import '../style/variables.scss';
+
+    nav {
+        height: calc(100% - 30px);
+        color: $gravel;
+        display: flex;
+        align-items: center;
+        background-color: green;
+
+        img {
+            height: 40px;
+        }
+
+        .navbar {
+            font-size: 14px;
+            font-weight: 600;
+
+            a {
+                text-transform: uppercase;    
+                color: $gravel;
+                margin: 0 10px;
+                padding: 10px 15px;
+            }
+        }
+    }
+
+</style>
